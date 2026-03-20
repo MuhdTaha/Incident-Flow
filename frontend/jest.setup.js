bc34e85e973815550796020f3b8d78532d921542
Muhammad Taha <muhd.taha@outlook.com>
2026-03-19 22:40:42 -0500

    chore: add requirements, add react-markdown dependency to package.json

--- frontend/jest.setup.js
+++ frontend/jest.setup.js
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom'
+
+jest.mock('react-markdown', () => {
+	const React = require('react');
+	return {
+		__esModule: true,
+		default: ({ children }) => React.createElement(React.Fragment, null, children),
+	};
+});
